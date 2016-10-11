701575bb74d996f26926258300b6032ea6aafc7f
Boris Däppen <boris_daeppen@bluewin.ch>
2016-10-11 16:02:23 +0200

    added HTML support

--- find_similar_python.pl
+++ find_similar_python.pl
@@ -40,6 +40,10 @@ for (my $i=0; $i < @files - 1; $i++) {
             ($cleaned_code1,
              $cleaned_code2) = prepare_php   ( $files[$i],  $files[$j] );
         }
+        if ($lang eq 'html') {
+            ($cleaned_code1,
+             $cleaned_code2) = prepare_html  ( $files[$i],  $files[$j] );
+        }
 
         my ($res, $diff) = measure( $cleaned_code1,  $cleaned_code2);
         say "$res\t$diff\t$files[$i]\t$files[$j]";
@@ -110,6 +114,41 @@ sub prepare_php {
       next if ($row =~ m!^/!);
       $row = $1 if ($row =~ m!(.*)//.*!);
       $row = $1 if ($row =~ m!(.*)/\*.*!);
+      $str2 .= $row
+    }
+    close $fh2;
+
+    # Whitespace raus
+    $str1 =~ s/\s*//g;
+    $str2 =~ s/\s*//g;
+
+    return ($str1, $str2);
+}
+
+sub prepare_html {
+    my $f1 = shift;
+    my $f2 = shift;
+
+    open(my $fh1, '<:encoding(UTF-8)', $f1)
+      or die "Could not open file '$f1' $!";
+     
+    open(my $fh2, '<:encoding(UTF-8)', $f2)
+      or die "Could not open file '$f2' $!";
+    
+    my $str1 = '';
+    while (my $row = <$fh1>) {
+      chomp $row;
+      next if ($row =~ m/^<!--/);
+      $row = $1 if ($row =~ m/(.*)<!--.*/);
+      $str1 .= $row
+    }
+    close $fh1;
+    
+    my $str2 = '';
+    while (my $row = <$fh2>) {
+      chomp $row;
+      next if ($row =~ m/^<!--/);
+      $row = $1 if ($row =~ m/(.*)<!--.*/);
       $str2 .= $row
     }
     close $fh2;
